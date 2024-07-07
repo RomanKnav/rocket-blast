c3a632c707b5df6ae7d3fd7ff9e5512ec6ac9eeb
Roman Navarro <ronnoverro@gmail.com>
2024-07-06 17:13:16 -0700

    init

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;      // the namespace that MonoBehavior lives in
+
+public class MovementScript : MonoBehaviour
+{
+    [SerializeField] float thrust = 1000f;
+    [SerializeField] float rotate = 500f;
+
+    private Rigidbody rb;    // MEMBER var: available everywhere in the class.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    // THERE'S A BUG IN HERE (I think: the log statements only log once?)
+    // actual bug: we don't wanna rotate left/right at the same time.
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) {
+            Debug.Log("Pressed Space -Thrusting");
+
+            // applies force RELATIVE to obj's position. Example, if facing right and pressed thrust, will move right instead of up.
+            // rb.AddRelativeForce(0, 1, 0);   // takes a vector3 as arg. Can also be written as:
+
+            rb.AddRelativeForce(thrust * Time.deltaTime * UnityEngine.Vector3.up);
+            // rb.AddRelativeForce(UnityEngine.Vector3.up);
+        }
+    }
+
+    void ProcessRotation() {
+
+        // only the z-axis tilt should change (for ship to tilt forward/back):
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Pressed A -Rotate Left");
+            ApplyRotation(rotate);
+        }
+
+        else if (Input.GetKey(KeyCode.D)) {     // ensures D is pressed only if A is not pressed
+            Debug.Log("Pressed D -Rotate Right");
+            ApplyRotation(-rotate);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        // freezing rotation so we can manually rotate (???)
+        rb.freezeRotation = true;   // how does this not freeze the entire rotation?
+        transform.Rotate(rotationThisFrame * Time.deltaTime * UnityEngine.Vector3.forward);
+        // rb.freezeRotation = false;  // unfreezing rotation so that physics system can take over.
+    }
+}
